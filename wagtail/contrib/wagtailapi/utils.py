87e16109d26c3ac1c0b7eb9f216f68f8220fc20e
Karl Hobley <karlhobley10@gmail.com>
2015-10-18 15:11:27 +0100

    dist/2015/07/31/wagtail-1.1.dev20150731-py2.py3-none-any

---
+++ wagtail/contrib/wagtailapi/utils.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.utils.six.moves.urllib.parse import urlparse
+
+
+class BadRequestError(Exception):
+    pass
+
+
+def get_base_url(request=None):
+    base_url = getattr(settings, 'WAGTAILAPI_BASE_URL', request.site.root_url if request else None)
+
+    if base_url:
+        # We only want the scheme and netloc
+        base_url_parsed = urlparse(base_url)
+
+        return base_url_parsed.scheme + '://' + base_url_parsed.netloc

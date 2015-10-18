8e2edfe2515d5068aa6bcd99f9cf48fff88c0f2d
Karl Hobley <karlhobley10@gmail.com>
2015-10-18 15:10:32 +0100

    dist/2015/08/02/wagtail-1.1.dev20150802-py2.py3-none-any

---
+++ wagtail/wagtailforms/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from wagtail.wagtailforms import views
+
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^submissions/(\d+)/$', views.list_submissions, name='list_submissions'),
+]

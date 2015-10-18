a66476c143f4e9e68c1b1ccfa56632f081e5b8b1
Karl Hobley <karlhobley10@gmail.com>
2015-10-18 15:11:31 +0100

    dist/2015/07/26/wagtail-1.1.dev20150726-py2.py3-none-any

---
+++ wagtail/wagtailsites/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from wagtail.wagtailsites import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^add/$', views.create, name='add'),
+    url(r'^(\d+)/$', views.edit, name='edit'),
+    url(r'^(\d+)/delete/$', views.delete, name='delete'),
+]
